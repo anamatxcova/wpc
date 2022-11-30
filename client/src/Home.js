fd9de72e60e3629fa906c68b3f2ec687061c099c
anamatxcova <anamatxcova@gmail.com>
2022-11-29 23:10:45 -0800

    fixed css

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,9 +3,11 @@ import React, {Component} from "react";
 export default class Home extends Component {
     render () {
         return (
-            <div className="auth-form-container">
-                <h2>Home</h2>
+            <div className="home-container">
+              <iframe title = "Charles was here" allow = "camera" style={{width: "1200px", height: "1200px", overflow: "hidden"}}  frameBorder="0" scrolling="no" src="https://charles-philip.github.io/Fancy-Fox/"></iframe>
             </div>
+            // <iframe title = "Charles was here" allow = "camera" style={{width: "1200px", height: "1200px", overflow: "hidden"}}  frameBorder="0" scrolling="no" src="https://editor.p5js.org/Charles-Philip/full/jDw8JAVyA"></iframe>
+        // <iframe src={perf}></iframe>
         )
     }
 }
