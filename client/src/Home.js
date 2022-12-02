ca370b2c87563690e478482344f16b9fd2b558d5
Hoai An Nguyen <71863184+anamatxcova@users.noreply.github.com>
2022-12-02 15:02:08 -0800

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,9 +3,9 @@ import React, {Component} from "react";
 export default class Home extends Component {
     render () {
         return (
-            <div className="home-container">
-              <iframe title = "Charles was here" allow = "camera" style={{width: "1200px", height: "1200px", overflow: "hidden"}}  frameBorder="0" scrolling="no" src="https://charles-philip.github.io/Fancy-Fox/"></iframe>
-            </div>
+            <><iframe title="Charles was here" allow="camera" style={{ width: "1200px", height: "480px", overflow: "hidden" }} frameBorder="0" scrolling="no" src="https://charles-philip.github.io/Fancy-Fox/"></iframe><footer>
+                <a href="https://www.flaticon.com/free-icons/yoga" title="yoga icons">Yoga icons created by Freepik - Flaticon</a>
+            </footer></>
             // <iframe title = "Charles was here" allow = "camera" style={{width: "1200px", height: "1200px", overflow: "hidden"}}  frameBorder="0" scrolling="no" src="https://editor.p5js.org/Charles-Philip/full/jDw8JAVyA"></iframe>
         // <iframe src={perf}></iframe>
         )
