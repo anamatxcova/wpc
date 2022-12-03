de5efd709aeffc7082022c30cd3515642618d7c9
Hoai An Nguyen <71863184+anamatxcova@users.noreply.github.com>
2022-12-03 00:35:56 -0800

    cleanup

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,8 +6,6 @@ export default class Home extends Component {
             <><iframe title="Charles was here" allow="camera" style={{ width: "1200px", height: "480px", overflow: "hidden" }} frameBorder="0" scrolling="no" src="https://charles-philip.github.io/Fancy-Fox/"></iframe><footer>
                 <a href="https://www.flaticon.com/free-icons/yoga" title="yoga icons">Yoga icons created by Freepik - Flaticon</a>
             </footer></>
-            // <iframe title = "Charles was here" allow = "camera" style={{width: "1200px", height: "1200px", overflow: "hidden"}}  frameBorder="0" scrolling="no" src="https://editor.p5js.org/Charles-Philip/full/jDw8JAVyA"></iframe>
-        // <iframe src={perf}></iframe>
         )
     }
 }
